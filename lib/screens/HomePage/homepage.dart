139f71c45dcbe078c10af1578f74d4ab33d76bb6
mohitrajsinha <mohitrajsinha321@gmail.com>
2023-02-24 17:55:53 +0530

    SELL feature added

--- lib/screens/HomePage/homepage.dart
+++ lib/screens/HomePage/homepage.dart
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
   var pages = [
     const Trending(),
     const Categories(),
-    const Sell(),
+     SELL(),
     const Recent(),
     const Profile()
   ];
--- lib/screens/HomePage/homepageItems/homePageItems.dart
+++ lib/screens/HomePage/homepageItems/homePageItems.dart
@@ -4,6 +4,7 @@ import 'package:olx_student_app/themes/themeDialogBox.dart';
 class HomePageItems {
   static AppBar HomePageAppBar() {
     return AppBar(
+      centerTitle: true,
       title: const Text("Home Page"),
       actions: const [
         Padding(
